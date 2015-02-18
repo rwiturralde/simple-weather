3736f482dce2f4b17829e3c86f9504364d13d183
Roberto Iturralde <robertoiturralde@RWI-MBA.local>
2015-02-17 21:01:19 -0500

    Initial Commit

---
+++ SimpleWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleWeather
+//
+//  Created by Roberto Iturralde on 2/17/15.
+//  Copyright (c) 2015 Roberto Iturralde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
